0c94ee3ce8ddce9a707217ca3614fa750824681b
fraczles <afracza1@binghamton.edu>
2015-03-29 23:27:59 -0400

    Defined PNN struct

--- pnn.go
+++ pnn.go
@@ -8,18 +8,34 @@ type PNN struct {
     // public double inputs[][];
     inputs [][]float64
     // public ArrayList<Double> classifications;
+    classifications []float64
     // public double correctClassifications[];
+    correctClassifications []float64
     // public File inputFile;
     // public File patternFile;
     // public double casePrior;
+    casePrior float64
     // public double contPrior;
+    contPrior float64
     // public double caseLoss;
+    caseLoss float64
     // public double contLoss;
+    contPrior float64
     // public ArrayList<Double> sigma;
+    sigma []float64
     // public int columns = 412;
+    columns int
     // public int count = 0;
+    count int
     // public int accum = 0;
+    accum int
     // public double bestAccuracy;
+    bestAccuracy float64
     // public double bestSigma;
+    bestSigma float64
+}
 
+func NewPNN() *PNN {
+    pnn := PNN{}
+    return &pnn
 }
