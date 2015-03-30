758384924399e27eb04597e68289ef0f1c1feb21
fraczles <afracza1@binghamton.edu>
2015-03-29 23:17:21 -0400

    Initial commit

---
+++ pnn.go
@@ -0,0 +1,25 @@
+package main
+
+type PNN struct {
+    // public double cases[][];
+    cases [][]float64
+    // public double controls[][];
+    controls [][]float64
+    // public double inputs[][];
+    inputs [][]float64
+    // public ArrayList<Double> classifications;
+    // public double correctClassifications[];
+    // public File inputFile;
+    // public File patternFile;
+    // public double casePrior;
+    // public double contPrior;
+    // public double caseLoss;
+    // public double contLoss;
+    // public ArrayList<Double> sigma;
+    // public int columns = 412;
+    // public int count = 0;
+    // public int accum = 0;
+    // public double bestAccuracy;
+    // public double bestSigma;
+
+}
